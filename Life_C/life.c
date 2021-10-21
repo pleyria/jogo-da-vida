5ff78098a0377d9273f135cd8b2d372f93c4c473
pleyria <pedroleyria@gmail.com>
2021-10-21 11:20:26 -0300

    commit

--- Life_C/life.c
+++ Life_C/life.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <omp.h>
 
+#define ALIVE 1
+#define DEAD 0
+
 /* Recebe um grid NxN.
  * Insere um glider a partir da posicao (0,0).
  * Insere um R-pentomino a partir da posicao (10, 30). */
@@ -26,12 +29,29 @@ void startGrid(int** grid, int N){
 /* Recebe um grid NxN.
  * Retorna o numero de vizinhos vivos de (i, j). */
 int getNeighbors(int** grid, int i, int j, int N){
+	int up, down, left, right, count;
 
+	up = i == 0 ? N-1 : i-1;
+	down = i == N-1 ? 0 : i + 1;
+	left = j == 0 ? N - 1: j - 1;
+	right = j == N - 1 ? 0 : j+1;
+
+	count = 0;
+	count += grid[up][j];
+	count += grid[up][right];
+	count += grid[i][right];
+	count += grid[down][right];
+	count += grid[down][j];
+	count += grid[down][left];
+	count += grid[i][left];
+	count += grid[up][left];
+
+	return count;
 }
 
 int main(void){
 	int N, geracoes;
-	int i;
+	int i, j;
 	int **grid, **newgrid;
 
 	// Leitura dos parametros
@@ -47,9 +67,18 @@ int main(void){
 		grid[i] = (int*) malloc(N * sizeof(int));
 		newgrid[i] = (int*) malloc(N * sizeof(int));
 	}
+	for(i=0; i<N; i++)
+		for(j=0; j<N; j++)
+			grid[i][j] = DEAD;
 
 	// Configuracao inicial do tabuleiro
 	startGrid(grid, N);
+
+	for(i=0; i<N; i++){
+		for(j=0; j<N; j++)
+			printf("%d ", grid[i][j]);
+		printf("\n");
+	}
 
 	// Liberacao da matriz NxN
 	for(i=0; i<N; i++){
